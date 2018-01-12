2129ebb01c225ad63accd4309da9e12ea7e6059d
4alit <35354743+4alit@users.noreply.github.com>
2018-01-12 07:57:29 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,73 @@
-Place your code here
+<!DOCTYPE html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+    "http://www.w3.org/TR/html4/strict.dtd">
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+    "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script>
+            $(function () {
+                var people = <?php
+echo json_encode(array(
+    array('id' => 1, 'first_name' => 'John', 'last_name' => 'Smith', 'email' => 'john.smith@hotmail.com'),
+    array('id' => 2, 'first_name' => 'Paul', 'last_name' => 'Allen', 'email' => 'paul.allen@microsoft.com'),
+    array('id' => 3, 'first_name' => 'James', 'last_name' => 'Johnston', 'email' => 'james.johnston@gmail.com'),
+    array('id' => 4, 'first_name' => 'Steve', 'last_name' => 'Buscemi', 'email' => 'steve.buscemi@yahoo.com'),
+    array('id' => 5, 'first_name' => 'Doug', 'last_name' => 'Simons', 'email' => 'doug.simons@hotmail.com')
+));
+?>;
+                //set table tag
+                var table = $('<table/>');
+                //append title and header
+                table.append('<caption><h1>Test application</h1></caption><tr><th>ID</th><th>First Name</th><th>Last Name</th><th>E-mail</th><th>Action</th></tr>');
+                //append people array into the table
+                $.each(people, function (i, value) {
+                    var row = $('<tr/>');
+                    $.each(value, function (k, v) {
+                        row.append($('<td/>').text(v));
+                    });
+                    row.append($('<button class="showRow">Click Me!</button>'));
+               table.append(row);
+                });
+
+                //add table to the page
+                $('.content').append(table);
+
+                $('.showRow').click(function () {
+                     var name = $(this).closest('tr').find('td:eq(1)').text();
+                     var email = $(this).closest('tr').find('td:eq(3)').text();
+                     alert('Name: ' + name+'\nEmail: ' + email);
+                });
+            });
+        </script>
+        <style>
+            table {
+                font-family: arial, sans-serif;
+                border-collapse: collapse;
+                width: 100%;
+            }
+
+            td, th {
+                border: 1px solid #dddddd;
+                text-align: left;
+                padding: 8px;
+            }
+
+            tr:nth-child(even) {
+                background-color: #dddddd;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="content">
+        </div>
+    </body>
+</html>
+
